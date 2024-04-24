ec5e508b0040a3c4b68c2adecc5ad552522bc623
FTMFR <fatemefarajzade018@gmail.com>
2024-04-24 22:48:33 +0330

    fix (SideBar): fix a bug about click on roues in SideBar component

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { BiCommentDetail } from "react-icons/bi";
 import { IoHomeOutline } from "react-icons/io5";
 import { BsBagCheck } from "react-icons/bs";
 import { FiUsers } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 import React from "react";
 import "./sideBar.css";
 
@@ -13,42 +13,30 @@ const Sidebar = () => {
     <div className="sidebar">
       <h1 className="sidebar-title">به داشبورد خود خوش آمدید.</h1>
       <ul className="sidebar-links">
-        <li>
-          <Link to="/products">
-            <IoHomeOutline className="icons" />
-            صفحه اصلی
-          </Link>
-        </li>
-        <li>
-          <Link to="/products">
-            <MdProductionQuantityLimits className="icons" />
-            محصولات
-          </Link>
-        </li>
-        <li>
-          <Link to="/comments">
-            <BiCommentDetail className="icons" />
-            کامنت ها
-          </Link>
-        </li>
-        <li>
-          <Link to="/orders">
-            <BsBagCheck className="icons" />
-            سفارشات
-          </Link>
-        </li>
-        <li>
-          <Link to="/users">
-            <FiUsers className="icons" />
-            کاربران
-          </Link>
-        </li>
-        <li>
-          <Link to="/discounts">
-            <RiDiscountPercentLine className="icons" />
-            تخفیفات
-          </Link>
-        </li>
+        <NavLink to="/">
+          <IoHomeOutline className="icons" />
+          صفحه اصلی
+        </NavLink>
+        <NavLink to="/products">
+          <MdProductionQuantityLimits className="icons" />
+          محصولات
+        </NavLink>
+        <NavLink to="/comments">
+          <BiCommentDetail className="icons" />
+          کامنت ها
+        </NavLink>
+        <NavLink to="/orders">
+          <BsBagCheck className="icons" />
+          سفارشات
+        </NavLink>
+        <NavLink to="/users">
+          <FiUsers className="icons" />
+          کاربران
+        </NavLink>
+        <NavLink to="/discounts">
+          <RiDiscountPercentLine className="icons" />
+          تخفیفات
+        </NavLink>
       </ul>
     </div>
   );
